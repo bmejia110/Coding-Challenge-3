05cab97791e3358e46e1af5bfe2802837be79911
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-08 18:23:37 -0400

    Test functions with sample sales data

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -69,3 +69,14 @@ function generateperformancereport(employeeSales) {
     return report  //returns the final report
 }
 
+//Task 5: Test Functions with Sample Data
+
+const salesData = [
+    {name: 'Alice' , sales: [12000, 15000, 13000] },
+    {name: 'Bob' , sales: [7000, 6000, 7500] },
+    {name: 'Charlie' , sales: [3000, 4000, 3500] },
+    {name: 'Diana' , sales: [9000, 8500, 9200] },
+];
+//testing function and logging the result
+let performancereport = generateperformancereport(employeeSales)
+console.log(performancereport);
